bd2033f743e06304755910734555ccb09dea56e4
Carlton Joseph <carlton.joseph@gmail.com>
2024-03-13 13:09:57 -0700

    add pwa image assets

--- vite.config.ts
+++ vite.config.ts
@@ -2,5 +2,40 @@ import { defineConfig } from "vite";
 import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
-  plugins: [VitePWA({ registerType: "autoUpdate" })],
+  plugins: [
+    VitePWA({
+      registerType: "autoUpdate",
+      includeAssets: ["logo.svg"],
+      manifest: {
+        name: "Text To Speech",
+        short_name: "t2s",
+        description: "Speaks Entered Text",
+        theme_color: "#ffffff",
+        icons: [
+          {
+            src: "pwa-64x64.png",
+            sizes: "64x64",
+            type: "image/png",
+          },
+          {
+            src: "pwa-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any",
+          },
+          {
+            src: "maskable-icon-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "maskable",
+          },
+        ],
+      },
+    }),
+  ],
 });
